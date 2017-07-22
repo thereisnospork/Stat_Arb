f141eaa1db149c58c86efff288406108146cbff7
George <georLeonard@gmail.com>
2017-07-22 14:07:56 -0700

    working on windows nothing special

--- Analysis.py
+++ Analysis.py
@@ -1,5 +1,18 @@
 import model as md
+import time
+import pandas as pd
+#import numpy as np #imports as dependency from model
 
-asdf = md.get_time_open('aapl',directory='/DJIA_minute')
-print(len(asdf))
+
+
+timing_start = time.time()
+#def trade_algo1_iter(up_then_down, symbol, buy_start = 5, n=20, money_init = 10000, buy_stop = 30, panic_stop = 30):
+
+asdf = md.trade_algo1_iter([0.07,.007],'gt',n=500,data_folder = './/DJIA_minute//')
+
 print(type(asdf))
+
+timing_end = time.time()
+timing = timing_end-timing_start
+print(timing)
+print(timing/500)
