ea89293ce062d1e1403bec44d78533b21b90a8e5
George <georLeonard@gmail.com>
2017-07-28 21:32:55 -0700

    second plot

--- Graphing.py
+++ Graphing.py
@@ -1,14 +1,16 @@
 from bokeh.plotting import figure, output_file, show
 import model as md
+import numpy as np
 
-sym = md.get_time_open('gt',directory='.//DJIA_2016//')
-df = md.trade_algo2((.007,.01),sym)
+sym = md.get_time_open('dis',directory='.//DJIA_2017//')
+df = md.trade_algo2_full_iter([ 0.01,  0.002],sym,panic_stop=60,iter = 200)
+df2 = md.trade_algo2_full_iter([ 0.01,  0.002],sym,panic_stop=60,iter = 200)
 
 df.to_csv('out.csv')
 
 print(type(df))
 
-plot_title = 'gt_2016'
+plot_title = '---'
 
 output_file(plot_title + '.html')
 x = df['day']
@@ -19,3 +21,20 @@ p.circle(x , y, fill_color='blue', size=3)
 
 show(p)
 
+output_file(plot_title + '.html')
+x = df['day']
+y = df['money']
+p2 = figure(title=plot_title, x_axis_label = 'days', y_axis_label = 'USD')
+p2.line(x, y, line_width=1)
+p2.circle(x , y, fill_color='blue', size=3)
+
+
+show(p)
+show(p2)
+print('up high')
+print(np.mean(df['profit']))
+print(np.std(df['profit']))
+print('down high')
+
+
+# [ 0.00326624,  0.00032132]
